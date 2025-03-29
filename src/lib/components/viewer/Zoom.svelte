9cff641b89c5a78a17b9ead30ea4577a211ddb3a
Chris Zubak-Skees <chriszs@gmail.com>
2025-03-29 15:18:04 +0400

    WIP button zoom

--- src/lib/components/viewer/Zoom.svelte
+++ src/lib/components/viewer/Zoom.svelte
@@ -5,6 +5,9 @@ Assumes it's a child of a ViewerContext
 -->
 <script lang="ts">
   import { _ } from "svelte-i18n";
+  import { ZoomIn16, ZoomOut16 } from "svelte-octicons";
+
+  import Button from "../common/Button.svelte";
 
   import { getDefaultZoom, getZoomLevels } from "@/lib/utils/viewer";
   import { getCurrentMode, getZoom } from "./ViewerContext.svelte";
@@ -14,21 +17,46 @@ Assumes it's a child of a ViewerContext
 
   $: zoomLevels = getZoomLevels($mode);
   $: $zoom = getDefaultZoom($mode);
+
+  function zoomIn() {
+    $zoom =
+      (zoomLevels.at(
+        zoomLevels.findIndex((level) => level[0] === $zoom) + 1,
+      )?.[0] as number) ?? 100;
+  }
+  function zoomOut() {
+    $zoom =
+      (zoomLevels.at(
+        zoomLevels.findIndex((level) => level[0] === $zoom) - 1,
+      )?.[0] as number) ?? 100;
+  }
 </script>
 
 {#if zoomLevels.length}
-  <label class="zoom">
-    {#if $mode === "grid"}
-      {$_("zoom.size")}
-    {:else}
-      {$_("zoom.zoom")}
-    {/if}
-    <select name="zoom" bind:value={$zoom}>
-      {#each zoomLevels as [value, label]}
-        <option {value}>{$_(label)}</option>
-      {/each}
-    </select>
-  </label>
+  <div class="zoomContainer">
+    <label class="zoom">
+      {#if $mode === "grid"}
+        {$_("zoom.size")}
+      {:else}
+        {$_("zoom.zoom")}
+      {/if}
+      <select name="zoom" bind:value={$zoom}>
+        {#each zoomLevels as [value, label]}
+          <option {value}>{$_(label)}</option>
+        {/each}
+      </select>
+    </label>
+    <Button ghost mode="primary" title="Zoom in" minW={false} on:click={zoomIn}
+      ><ZoomIn16 /></Button
+    >
+    <Button
+      ghost
+      mode="primary"
+      title="Zoom out"
+      minW={false}
+      on:click={zoomOut}><ZoomOut16 /></Button
+    >
+  </div>
 {/if}
 
 <style>
@@ -58,5 +86,12 @@ Assumes it's a child of a ViewerContext
     font-family: var(--font-sans);
     font-size: var(--font-md);
     box-shadow: none;
+  }
+
+  .zoomContainer {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    justify-content: flex-end;
   }
 </style>
